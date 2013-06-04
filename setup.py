8a51825934eec763c04c1488f99d69cbc521fbb9
Lyman Gillispie <lyman.gillispie@gmail.com>
2013-06-03 16:56:19 -0800

    moved to setuptools

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='femToybox',
-    version='0.1.2-0.0.1',
+    version='0.1.2-0.0.2',
     author='Lyman Gillispie',
     author_email="lyman.gillispie@gmail.com",
     url="https://github.com/fmuzf/python-femToybox",
-    packages=['femtoybox'],
+    packages=find_packages(),
     install_requires=[
         "numpy >= 1.7.0",
         "scipy >= 0.10"
