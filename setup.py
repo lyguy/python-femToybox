f68914d7d37065ec7c087c281ad7b0a46c00a6d3
Lyman Gillispie <lyman.gillispie@gmail.com>
2013-06-05 13:36:45 -0800

    bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='femToybox',
-    version='0.1.2-0.0.2',
+    version='0.1.2',
     author='Lyman Gillispie',
     author_email="lyman.gillispie@gmail.com",
     url="https://github.com/fmuzf/python-femToybox",
