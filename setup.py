65ab43e22a5401f0b58130447761174f998a5ed4
Lyman Gillispie <lyman.gillispie@gmail.com>
2013-08-13 14:32:07 -0800

    updated version

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='femToybox',
-    version='0.1.2',
+    version='0.1.3-0.0.2',
     author='Lyman Gillispie',
     author_email="lyman.gillispie@gmail.com",
     url="https://github.com/fmuzf/python-femToybox",
     packages=find_packages(),
+    license='MIT',
     install_requires=[
         "numpy >= 1.7.0",
         "scipy >= 0.10"
-    ]
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose']
 )
